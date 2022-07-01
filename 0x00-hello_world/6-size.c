75b2d8dc4f0abd794c481f950a2bda113b90183a
Uwadamsel <p.uwadiae460@gmail.com>
2022-07-01 07:03:56 -0700

    Task 4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-int main(void)
-{
-	int intType;
-	float floatType;
-	double doubleType;
-	char charType;
-
-	sizeof(evaluates) the size of a variable
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
-	printf("Size of char: %zu byte\n", sizeof(charType));
-
-	return (0);
-}
+1 int main(void)
+2 {
+3	int intType;
+4	float floatType;
+5	double doubleType;
+6	char charType;
+7
+8	sizeof(evaluates) the size of a variable
+9	printf("Size of int: %zu bytes\n", sizeof(intType));
+10	printf("Size of float: %zu bytes\n", sizeof(floatType));
+11	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+12	printf("Size of char: %zu byte\n", sizeof(charType));
+13
+14	return (0);
+15 }
