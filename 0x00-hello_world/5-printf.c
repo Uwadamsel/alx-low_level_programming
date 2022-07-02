633fef6c5f593e2639c0144a88914808913363a8
Uwadamsel <p.uwadiae460@gmail.com>
2022-07-02 08:54:27 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,7 +9,6 @@
 */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art");
-	printf("hello_world!");
+	printf("\"with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
