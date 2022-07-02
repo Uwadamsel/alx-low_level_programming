78d892939ea8979ba8216d6423113b5e808d067d
Uwadamsel <p.uwadiae460@gmail.com>
2022-07-02 06:34:36 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /*
 * main - Entry point
+*
+* Description - write C program that prints
 * Return: Always 0 (Success)
 */
-int main(void);
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingua	l puzzle\n");
 	return (0);
 }
