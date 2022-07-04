e8a61e6190d3f4e0e757ce55637ff8d141d72df9
Uwadamsel <p.uwadiae460@gmail.com>
2022-07-04 06:06:50 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,8 +5,6 @@
 /**
  * main -> assign a random number to the variable n each time it is executed
  * based a condition
- * description: Positive anything is better than negative nothing
- *
  * Return : always 0
 */
 int main(void)
