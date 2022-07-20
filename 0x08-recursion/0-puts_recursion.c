2f954326b0d87e08899a78d640ee931da9b5ca10
Uwadamsel <p.uwadiae460@gmail.com>
2022-07-20 08:35:04 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _puts_recursion - print a string
  * @s: string
@@ -7,7 +8,7 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if(*s)
 	{
 		_putchar(*s)
 		_puts_recursion(s + 1);
