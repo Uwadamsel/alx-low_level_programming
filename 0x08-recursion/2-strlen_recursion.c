2eb85fee6d160f893dac1fd66d7ef112ba042f9d
Uwadamsel <p.uwadiae460@gmail.com>
2022-07-20 19:34:03 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ * Return 0;
+*/
+int main(void)
+{
+	char s[] = "Programming is fun";
+	int i;
+
+	for
+		(i = 0; s[i] != '\0'; ++i);
+
+	printf("Length of the string: %d", i);
+	Return 0;
+}
